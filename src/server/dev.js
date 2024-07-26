272baedab516c2d766c17f0059e97a718094c62a
David Konsumer <konsumer@jetboystudio.com>
2024-07-26 05:20:16 -0700

    seperate server

--- src/server/dev.js
+++ src/server/dev.js
@@ -1,5 +1,5 @@
 // this will load a local dev-server
 import 'dotenv/config'
 import ViteExpress from "vite-express"
-import app from './server.js'
+import app from './app.js'
 ViteExpress.listen(app, 3000, () => console.log("Server is listening on http://localhost:3000/"))
